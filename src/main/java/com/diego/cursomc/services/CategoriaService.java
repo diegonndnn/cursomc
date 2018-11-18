b7b0d16fd5b5568c62d23d5eb697a3b6bf746d39
Diego Marques <javanatal@gmail.com>
2018-11-18 14:40:12 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/diego/cursomc/services/CategoriaService.java
+++ src/main/java/com/diego/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.diego.cursomc.domain.Categoria;
 import com.diego.cursomc.repositories.CategoriaRepository;
+import com.diego.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,8 +18,14 @@ public class CategoriaService {
 	
 	//Metodo ajustado conforme atualizacao para Spring Boot 2)
 	public Categoria buscar(Integer id) {
+		/**Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); */
+				
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+ id + ", Tipo: " + Categoria.class.getName()));
+		
+		
 	}
 	
 }
