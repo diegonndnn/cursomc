95cbd50dde90d6e9ba9a6627ce88524a0ae13fc0
Diego Marques <javanatal@gmail.com>
2018-11-18 00:55:16 -0300

    instanciacao de categoria na inicializacao do sistema

--- src/main/java/com/diego/cursomc/CursomcApplication.java
+++ src/main/java/com/diego/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.diego.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.diego.cursomc.domain.Categoria;
+import com.diego.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		//categoriaRepository.saveAll conforme SP2
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+
 }
